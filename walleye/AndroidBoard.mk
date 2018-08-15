7748f8ec36e83ebb1c8f0da6b672e0133512cc8b
mailoxy <roman@mailoxy.com>
2018-08-15 22:59:48 +0300

    Change product name and switch to monolithic kernel

--- walleye/AndroidBoard.mk
+++ walleye/AndroidBoard.mk
@@ -1,1 +1,16 @@
-../AndroidBoard.mk
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+KERNEL_DEFCONFIG := walleye-debug_defconfig
+else
+KERNEL_DEFCONFIG := walleye_defconfig
+endif
+KERNEL_DIR := kernel/google/wahoo
+TARGET_KERNEL_APPEND_DTB := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CLANG_TRIPLE := aarch64-linux-gnu-
+
+include $(TOP)/$(KERNEL_DIR)/AndroidKernel.mk
+
+.PHONY: $(PRODUCT_OUT)/kernel
+$(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
+	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
